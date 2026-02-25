824aa3c0befa99fc3454b7685cfa08506b24fb4e
Elvis Oliveira <elvis.olv@gmail.com>
2026-02-25 10:56:28 +0100

    Add Extranet launcher and update EPOS Web script; enhance desktop icons and Chromium policies

--- config/includes.chroot/etc/skel/extranet.sh
+++ config/includes.chroot/etc/skel/extranet.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 chromium_version=`chromium --product-version`
 user_agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chrome/$chromium_version $HOSTNAME ab"
-tmpdir=/tmp/eposnextweb_nl
+tmpdir=/tmp/chrome_extranet
 trap "rm -rf $tmpdir" EXIT
 mkdir $tmpdir
 touch "$tmpdir/First Run"
-targetURL="https://epos-web-next.gke.internal.toolstation.nl/"
-chromium --user-agent="$user_agent" --user-data-dir=$tmpdir --password-store=basic --disable-print-preview --start-maximized --app=$targetURL
+targetURL="https://extranet.toolstation.nl/v2.1/index.html"
+chromium --user-agent="$user_agent" --user-data-dir=$tmpdir --password-store=basic --disable-print-preview --window-size=1024,1024 --app=$targetURL
